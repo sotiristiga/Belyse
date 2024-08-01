65389ca65fa1dcacbd5269af3861ac73326ee5ba
sotiristiga <99202968+sotiristiga@users.noreply.github.com>
2024-08-01 12:22:26 +0300

    Update Belyse.py

--- Belyse.py
+++ Belyse.py
@@ -10,7 +10,16 @@ import streamlit as st
 from math import ceil
 from datetime import date
 from streamlit_dynamic_filters import DynamicFilters
-st.set_page_config(layout='wide',page_title="Belyse")
+import urllib.request
+from PIL import Image
+
+
+def download_image(url, save_as):
+    urllib.request.urlretrieve(url, save_as)
+
+download_image('https://belyse.gr/image/cache/catalog/assets/logos/logo-708x278.png','belyse.png')
+
+st.set_page_config(layout='wide',page_title="Belyse Lights")
 belyse= pd.read_csv(f"https://raw.githubusercontent.com/sotiristiga/Belyse/main/Belyse.csv")
 belyse['entry_date'] = pd.to_datetime(belyse['entry_date'],dayfirst=True)
 belyse['Date_order']=belyse["entry_date"].dt.date
@@ -28,13 +37,14 @@ belyse['Month_Year']=pd.to_datetime(belyse['Month_Year'],format='mixed')
 belyse['Month_order']=pd.Categorical(belyse['Month_order'],categories=month_levels)
 belyse['Product']=belyse['Product'].replace('� ','')
 belyse['MPN']="C-"+ belyse['MPN']
+belyse['Total_Price']=belyse['Price']*belyse['Quantity']
 belyse_complete_orders=belyse.loc[(belyse['order_situation']=="Ολοκληρώθηκε")|((belyse['order_situation']=="Μέρος της έχει επιστραφεί")& (belyse['Return']==0))]
 Quantity_products=pd.merge(belyse_complete_orders[['MPN','Product']].value_counts().reset_index(),belyse_complete_orders.groupby(['MPN','Product'])['Quantity'].sum().reset_index())
 Quantity_products['Total_quantity']=Quantity_products['Quantity']*Quantity_products['count']
 
 lnk = '<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.12.1/css/all.css" crossorigin="anonymous">'
-def metrics_customize(iconname,sline,i):
-    htmlstr = f"""<p style='background-color: rgb(0,204,102, 0.75); 
+def metrics_customize(red,green,blue,iconname,sline,i):
+    htmlstr = f"""<p style='background-color: rgb({red},{green},{blue}, 0.75); 
                         color: rgb(0,0,0, 0.75); 
                         font-size: 25px; 
                         border-radius: 7px; 
@@ -46,27 +56,37 @@ def metrics_customize(iconname,sline,i):
                         </style><BR><span style='font-size: 22px; 
                         margin-top: 0;'>{sline}</style></span></p>"""
     return htmlstr
-
-kpi1,kpi2,kpi3,kpi4,kpi5,kpi6,kpi7 = st.columns(7)
+st.image(Image.open("belyse.png"),width=400 )
+kpi1,kpi2,kpi3,kpi4= st.columns(4)
 with kpi1:
-    st.markdown(lnk + metrics_customize("fas fa-shopping-cart","Ολοκληρωμένες Παραγγελίες",belyse_complete_orders['id_order'].nunique()), unsafe_allow_html=True)
+    st.markdown(lnk + metrics_customize(0,204,102,"fas fa-shopping-cart","Ολοκληρωμένες Παραγγελίες",belyse_complete_orders['id_order'].nunique()), unsafe_allow_html=True)
 with kpi1:
-    st.markdown(lnk + metrics_customize("far fa-lightbulb","Συνολικά προϊόντα που πουλήθηκαν",belyse_complete_orders['id'].nunique()), unsafe_allow_html=True)
+    st.markdown(lnk + metrics_customize(0,204,102,"far fa-lightbulb","Συνολικά προϊόντα που πουλήθηκαν",belyse_complete_orders['id'].nunique()), unsafe_allow_html=True)
+with kpi1:
+    st.markdown(lnk + metrics_customize(0,204,102, "far fa-lightbulb", "Κωδικοί προϊόντων που πουλήθηκαν",
+                                        belyse_complete_orders['MPN'].nunique()),
+                unsafe_allow_html=True)
+with kpi2:
+    st.markdown(lnk + metrics_customize(233,58,28,"fas fa-ban","Aκυρωμένες Παραγγελίες",belyse[belyse['order_situation']=="Ακυρωμένη"]['id_order'].nunique()), unsafe_allow_html=True)
+with kpi2:
+    st.markdown(lnk + metrics_customize(255,192,0,"fas fa-times","Παραγγελίες που απορρίφθηκαν",belyse[belyse['order_situation']=="Απορρίφθηκε"]['id_order'].nunique()), unsafe_allow_html=True)
+with kpi2:
+    st.markdown(lnk + metrics_customize(21,179,235,"fas fa-backward","Επιστροφές Προϊόντών",belyse['Return'].sum()), unsafe_allow_html=True)
 
-kpi2.metric(label="Aκυρωμένες Παραγγελίες",
-        value=belyse[belyse['order_situation']=="Ακυρωμένη"][['id_order']].nunique())
-kpi3.metric(label="Παραγγελίες που απορρίφθηκαν",
-        value=belyse[belyse['order_situation']=="Απορρίφθηκε"][['id_order']].nunique())
-kpi4.metric(label="Επιστροφή Προϊόντών",
-        value=belyse['Return'].sum())
-kpi5.metric(label="Έσοδα απο πωλήσεις",
-        value=belyse_complete_orders['Total_payment'].sum().round(2))
-kpi6.metric(label="Προμήθεια Skroutz",
-        value=belyse_complete_orders.groupby('id_order')['Skroutz_Commission'].mean().reset_index()['Skroutz_Commission'].sum().round(2))
-kpi7.metric(label="Αρ. προϊόντων ανά παραγγελία",
-        value=belyse_complete_orders['id_order'].value_counts().reset_index()['count'].mean().round(1))
+with kpi3:
+    st.markdown(lnk + metrics_customize(20, 102, 120, "fas fa-user", "Προμηθευτές",
+                                        belyse_complete_orders['Supplier'].nunique()),
+                unsafe_allow_html=True)
+with kpi3:
+    st.markdown(lnk + metrics_customize(0,204,102,"fas fa-euro-sign","Έσοδα απο πωλήσεις",belyse_complete_orders['Total_Price'].sum().round(2)), unsafe_allow_html=True)
+with kpi3:
+    st.markdown(lnk + metrics_customize(0,204,102,"fas fa-euro-sign","Προμήθεια Skroutz",belyse_complete_orders.groupby('id_order')['Skroutz_Commission'].mean().reset_index()['Skroutz_Commission'].sum().round(2)), unsafe_allow_html=True)
+with kpi4:
+    st.markdown(lnk + metrics_customize(0,204,102,"fas fa-shopping-bag","Αρ. προϊόντων ανά παραγγελία",belyse_complete_orders['id_order'].value_counts().reset_index()['count'].mean().round(1)), unsafe_allow_html=True)
 
-top41, top42 = st.columns(2)
+
+st.write('#### Top5 προϊόντα')
+top41, top42,top43 = st.tabs(["Σε πωλήσεις","Σε ζήτηση ποσότητας","Σε έσοδα απο τις πωλησεις"])
 with top41:
     fig_line_comp_orders_mbm = px.bar(
         belyse_complete_orders[['Product','MPN']].value_counts().head(5).reset_index().sort_values('count'),
@@ -74,8 +94,8 @@ with top41:
         title='Top 5 προϊόντα σε πωλήσεις',hover_data=['Product','MPN'],
         labels={'MPN': 'Κωδ. Προϊόντος', "count": 'Συνολικές πωλήσεις'},
         text='count', width=1200, height=400)
-    fig_line_comp_orders_mbm.update_traces(textfont_size=16, textangle=0, marker_color='#8690FF', cliponaxis=False,
-                                           textposition='inside')
+    fig_line_comp_orders_mbm.update_traces(textfont_size=16, textangle=0, marker_color='#146678', cliponaxis=False,
+                                           textposition='outside')
     fig_line_comp_orders_mbm.update_layout(plot_bgcolor='white', font_size=8)
     st.write(fig_line_comp_orders_mbm)
 with top42:
@@ -85,11 +105,21 @@ with top42:
         title='Top 5 προϊόντα σε ποσότητες προϊόντων',hover_data=['Product','MPN'],
         labels={'MPN': 'Κωδ. Προϊόντος', "Quantity": 'Ποσότητα'},
         text='Quantity', width=1200, height=400)
-    fig_line_comp_orders_mbm.update_traces(textfont_size=16, textangle=0, marker_color='#8690FF', cliponaxis=False,
-                                           textposition='inside')
+    fig_line_comp_orders_mbm.update_traces(textfont_size=16, textangle=0, marker_color='#146678', cliponaxis=False,
+                                           textposition='outside')
     fig_line_comp_orders_mbm.update_layout(plot_bgcolor='white', font_size=8)
     st.write(fig_line_comp_orders_mbm)
-
+with top43:
+    fig_line_comp_orders_mbm = px.bar(
+        belyse_complete_orders.groupby(['Product','MPN'])['Total_Price'].sum().reset_index().sort_values('Total_Price',ascending=False).head(5).sort_values('Total_Price').round(2),
+        x="Total_Price", y="MPN",
+        title='',hover_data=['Product','MPN'],
+        labels={'MPN': 'Κωδ. Προϊόντος', "Total_Price": 'Έσοδα'},
+        text='Total_Price', width=1200, height=400)
+    fig_line_comp_orders_mbm.update_traces(textfont_size=16, textangle=0, marker_color='#146678', cliponaxis=False,
+                                           textposition='outside')
+    fig_line_comp_orders_mbm.update_layout(plot_bgcolor='white', font_size=8)
+    st.write(fig_line_comp_orders_mbm)
 
 
 
@@ -104,6 +134,7 @@ with tab1:
                                         title='',
                                 labels={'Date_order':'Ημερομηνία',"count":'Ολοκληρωμένες παραγγελίες'},
                                 markers=True,width=1800)
+                fig_line_comp_orders_dbd.update_traces(marker_color='#146678',line_color="#F0BF4C")
                 fig_line_comp_orders_dbd.update_layout(plot_bgcolor='white',font_size=13)
                 st.write(fig_line_comp_orders_dbd)
         with tab12:
@@ -112,6 +143,7 @@ with tab1:
                                         title='',
                                 labels={'Month_Year':'Μήνας-Έτος',"count":'Ολοκληρωμένες παραγγελίες'},
                                 text_auto=True,width=1000)
+                fig_line_comp_orders_mbm.update_traces(marker_color='#146678')
                 fig_line_comp_orders_mbm.update_layout(plot_bgcolor='white',font_size=13)
                 st.write(fig_line_comp_orders_mbm)
         with tab13:
@@ -128,74 +160,36 @@ with tab2:
     tab21, tab22, tab23 = st.tabs(['Ανά ημέρα', "Ανά μήνα", 'Σύγκριση ανά έτος'])
     with tab21:
         fig_line_comp_orders_dbd = px.line(
-            belyse_complete_orders.groupby(['Date_order', 'id_order'])['Price'].mean().reset_index().groupby(
-                'Date_order')['Price'].sum().reset_index().sort_values('Date_order'),
-            x="Date_order", y="Price",
+            belyse_complete_orders.groupby(['Date_order', 'id_order'])['Total_Price'].mean().reset_index().groupby(
+                'Date_order')['Total_Price'].sum().reset_index().sort_values('Date_order'),
+            x="Date_order", y="Total_Price",
             title='',
             labels={'Date_order': 'Ημερομηνία', "Price": 'Έσοδα από πωλήσεις'},
             markers=True, width=1800)
+        fig_line_comp_orders_dbd.update_traces(marker_color='#146678', line_color="#F0BF4C")
         fig_line_comp_orders_dbd.update_layout(plot_bgcolor='white', font_size=13)
         st.write(fig_line_comp_orders_dbd)
     with tab22:
         fig_line_comp_orders_mbm = px.bar(
-            belyse_complete_orders.groupby(['Month_Year', 'id_order'])['Price'].mean().reset_index().groupby(
-                'Month_Year')['Price'].sum().reset_index().sort_values('Month_Year'),
-            x="Month_Year", y="Price",
+            belyse_complete_orders.groupby(['Month_Year', 'id_order'])['Total_Price'].mean().reset_index().groupby(
+                'Month_Year')['Total_Price'].sum().reset_index().sort_values('Month_Year'),
+            x="Month_Year", y="Total_Price",
             title='',
-            labels={'Month_Year': 'Μήνας-Έτος', "Price": 'Έσοδα από πωλήσεις'},
-            text='Price', width=1000)
+            labels={'Month_Year': 'Μήνας-Έτος', "Total_Price": 'Έσοδα από πωλήσεις'},
+            text='Total_Price', width=1000)
         fig_line_comp_orders_mbm.update_traces(textfont_size=13, texttemplate='%{text:.3s} €', textangle=0,
-                                               cliponaxis=False)
+                                               cliponaxis=False,marker_color='#146678')
         fig_line_comp_orders_mbm.update_layout(plot_bgcolor='white', font_size=12)
         st.write(fig_line_comp_orders_mbm)
     with tab23:
-        fig_line_comp_orders_yby = px.line(belyse_complete_orders.groupby(['Month_order', 'Year_order'])['Price'].sum().reset_index().sort_values(
+        fig_line_comp_orders_yby = px.line(belyse_complete_orders.groupby(['Month_order', 'Year_order'])['Total_Price'].sum().reset_index().sort_values(
                 'Month_order'),
-            x="Month_order", y="Price",
+            x="Month_order", y="Total_Price",
             title='', color='Year_order',
             labels={'Month_order': 'Ημερομηνία', "Price": 'Έσοδα από πωλήσεις','Year_order':'Έτος'},
             markers=True, width=1800)
         fig_line_comp_orders_yby.update_layout(plot_bgcolor='white', font_size=13)
         st.write(fig_line_comp_orders_yby)
-
-
-st.write("#### Προμηθευτές")
-
-col1,col2,col3=st.columns(3)
-with col1:
-    fig_line_comp_orders_mbm = px.bar(
-        belyse_complete_orders[['Supplier']].value_counts().reset_index().groupby('Supplier')['count'].sum().reset_index().sort_values('count'),
-        x="Supplier", y="count",
-        title='Συνολικές παραγγελίες',
-        labels={'Supplier': 'Προμηθευτής', "count": 'Αρ. παρραγγελίων'},
-        text='count', width=1000)
-    fig_line_comp_orders_mbm.update_traces(textfont_size=13,  textangle=0,
-                                           cliponaxis=False)
-    fig_line_comp_orders_mbm.update_layout(plot_bgcolor='white', font_size=12)
-    st.write(fig_line_comp_orders_mbm)
-with col2:
-    fig_line_comp_orders_mbm = px.bar(
-        belyse_complete_orders.groupby('Supplier')['Price'].sum().reset_index().sort_values('Price'),
-        x="Supplier", y="Price",
-        title='Συνολική παραγωγή σε πωλήσεις',
-        labels={'Supplier': 'Προμηθευτής', "Price": 'Πωλήσεις €'},
-        text='Price', width=1000)
-    fig_line_comp_orders_mbm.update_traces(textfont_size=13, texttemplate='%{text:.2s} €', textangle=0,
-                                           cliponaxis=False)
-    fig_line_comp_orders_mbm.update_layout(plot_bgcolor='white', font_size=12)
-    st.write(fig_line_comp_orders_mbm)
-
-with col3:
-    fig_line_comp_orders_mbm = px.bar(
-        belyse_complete_orders.groupby('Supplier')['Quantity'].sum().reset_index().sort_values('Quantity'),
-        x="Supplier", y="Quantity",
-        title='Συνολική παραγωγή σε ζήτηση ποσοτήτων',
-        labels={'Supplier': 'Προμηθευτής', "Quantity": 'Ποσότητα'},
-        text='Quantity', width=1000)
-    fig_line_comp_orders_mbm.update_traces(textfont_size=13,  textangle=0,
-                                           cliponaxis=False)
-    fig_line_comp_orders_mbm.update_layout(plot_bgcolor='white', font_size=12)
-    st.write(fig_line_comp_orders_mbm)
 
 pie1,pie2,pie3= st.columns(3)
 
@@ -231,7 +225,43 @@ with pie3:
     pie3.update_layout(plot_bgcolor='white',font_size=20,legend=dict(yanchor="top",y=0.65,xanchor="right",x=0.1),title_y=0.6,title_x=0.1)
     st.write(pie3)
 
+st.write("#### Προμηθευτές")
 
+col1,col2,col3=st.columns(3)
+with col1:
+    fig_line_comp_orders_mbm = px.bar(
+        belyse_complete_orders[['Supplier']].value_counts().reset_index().groupby('Supplier')['count'].sum().reset_index().sort_values('count'),
+        x="Supplier", y="count",
+        title='Συνολικές παραγγελίες',
+        labels={'Supplier': 'Προμηθευτής', "count": 'Αρ. παρραγγελίων'},
+        text='count', width=1000)
+    fig_line_comp_orders_mbm.update_traces(textfont_size=13,  textangle=0,
+                                           cliponaxis=False,marker_color='#146678')
+    fig_line_comp_orders_mbm.update_layout(plot_bgcolor='white', font_size=12)
+    st.write(fig_line_comp_orders_mbm)
+with col2:
+    fig_line_comp_orders_mbm = px.bar(
+        belyse_complete_orders.groupby('Supplier')['Total_Price'].sum().reset_index().sort_values('Total_Price'),
+        x="Supplier", y="Total_Price",
+        title='Συνολική παραγωγή σε πωλήσεις',
+        labels={'Supplier': 'Προμηθευτής', "Total_Price": 'Πωλήσεις €'},
+        text='Total_Price', width=1000)
+    fig_line_comp_orders_mbm.update_traces(textfont_size=13, texttemplate='%{text:.2s} €', textangle=0,
+                                           cliponaxis=False,marker_color='#146678')
+    fig_line_comp_orders_mbm.update_layout(plot_bgcolor='white', font_size=12)
+    st.write(fig_line_comp_orders_mbm)
+
+with col3:
+    fig_line_comp_orders_mbm = px.bar(
+        belyse_complete_orders.groupby('Supplier')['Quantity'].sum().reset_index().sort_values('Quantity'),
+        x="Supplier", y="Quantity",
+        title='Συνολική παραγωγή σε ζήτηση ποσοτήτων',
+        labels={'Supplier': 'Προμηθευτής', "Quantity": 'Ποσότητα'},
+        text='Quantity', width=1000)
+    fig_line_comp_orders_mbm.update_traces(textfont_size=13,  textangle=0,
+                                           cliponaxis=False,marker_color='#146678')
+    fig_line_comp_orders_mbm.update_layout(plot_bgcolor='white', font_size=12)
+    st.write(fig_line_comp_orders_mbm)
 
 
 
